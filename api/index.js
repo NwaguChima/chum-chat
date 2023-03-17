7cfdfea805bfa7d99006a40440714cbd971e3f1e
NwaguChima <nwaguchima48@gmail.com>
2023-03-17 02:44:45 +0100

    added try catch to register endpoint

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const User = require('./models/User');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -17,30 +18,43 @@ mongoose
   });
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 
 app.get('/test', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
 
-app.post('./register', async (req, res) => {
+app.post('/register', async (req, res) => {
   const { username, password } = req.body;
 
-  const user = await User.create({
-    username,
-    password,
-  });
+  try {
+    const user = await User.create({
+      username,
+      password,
+    });
 
-  jwt.sign({ userId: user._id }, process.env.JWT_SECRET, (err, token) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ message: 'Error signing token', error: err });
-    }
+    jwt.sign({ userId: user._id }, process.env.JWT_SECRET, (err, token) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Error signing token', error: err });
+      }
 
-    res
-      .cookie('token', token)
-      .sendStatus(201)
-      .json({ message: 'User created successfully' });
-  });
+      res
+        .cookie('token', token)
+        .sendStatus(201)
+        .json({ message: 'User created successfully' });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error registering user', error });
+  }
 });
 
 app.listen(4000, () => {
