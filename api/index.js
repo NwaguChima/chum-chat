0355955de852c491a17ef324127c72549a947fa2
NwaguChima <nwaguchima48@gmail.com>
2023-03-17 02:19:59 +0100

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.log(err);
+});
 
 const app = express();
 
 app.get('/test', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
+
+app.post('./register', (req, res) => {});
 
 app.listen(4000, () => {
   console.log('Server listening on port 4000');
