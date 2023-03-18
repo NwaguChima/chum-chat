3df5ee16e4ef288832eace984db6aef6ac4af1f2
NwaguChima <nwaguchima48@gmail.com>
2023-03-18 08:39:16 +0100

    implemented login endpoint

---
+++ api/controllers/userController.js
@@ -0,0 +1,96 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+
+const profile = async (req, res) => {
+  const token = req.cookies?.token;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, {}, (err, userData) => {
+      if (err) throw err;
+
+      res.json(userData);
+    });
+  } else {
+    res.status(401).json('no token');
+  }
+};
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const foundUser = await User.findOne({ username });
+
+    if (!foundUser) {
+      res.status(401).json('no user');
+    }
+
+    const isMatch = bcrypt.compareSync(password, foundUser.password);
+
+    if (!isMatch) {
+      res.status(401).json('no match');
+    }
+
+    jwt.sign(
+      { userId: foundUser._id, username },
+      process.env.JWT_SECRET,
+      (err, token) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: 'Error signing token', error: err });
+        }
+
+        res
+          .cookie('token', token, { sameSite: 'none', secure: true })
+          .status(201)
+          .json({
+            id: foundUser._id,
+          });
+      }
+    );
+  } catch (error) {}
+};
+
+const register = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+
+    const user = await User.create({
+      username,
+      password: hashedPassword,
+    });
+
+    jwt.sign(
+      { userId: user._id, username },
+      process.env.JWT_SECRET,
+      (err, token) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: 'Error signing token', error: err });
+        }
+
+        res
+          .cookie('token', token, { sameSite: 'none', secure: true })
+          .status(201)
+          .json({
+            id: user._id,
+            message: 'User created successfully',
+          });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error registering user', error });
+  }
+};
+
+module.exports = {
+  profile,
+  login,
+  register,
+};
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,58 +1,12 @@
 const express = require('express');
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.get('/profile', (req, res) => {
-  const token = req.cookies?.token;
+router.get('/profile', userController.profile);
 
-  if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, {}, (err, userData) => {
-      if (err) throw err;
+router.post('/login', userController.login);
 
-      res.json(userData);
-    });
-  } else {
-    res.status(401).json('no token');
-  }
-});
-
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-});
-
-router.post('/register', async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = await User.create({
-      username,
-      password,
-    });
-
-    jwt.sign(
-      { userId: user._id, username },
-      process.env.JWT_SECRET,
-      (err, token) => {
-        if (err) {
-          console.log(err);
-          res.status(500).json({ message: 'Error signing token', error: err });
-        }
-
-        res
-          .cookie('token', token, { sameSite: 'none', secure: true })
-          .status(201)
-          .json({
-            id: user._id,
-            message: 'User created successfully',
-          });
-      }
-    );
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: 'Error registering user', error });
-  }
-});
+router.post('/register', userController.register);
 
 module.exports = router;
