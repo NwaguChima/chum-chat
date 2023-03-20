c0003b23ee71a6ded6fdd44a3cf4ede519cd4bd5
NwaguChima <nwaguchima48@gmail.com>
2023-03-20 19:53:58 +0100

    styled avatar

---
+++ client/src/components/Avatar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Avatar = ({ userId, username }) => {
+  const colors = [
+    'bg-red-200',
+    'bg-blue-200',
+    'bg-green-200',
+    'bg-yellow-200',
+    'bg-purple-200',
+    'bg-pink-200',
+    'bg-indigo-200',
+    'bg-gray-200',
+    'bg-teal-200',
+  ];
+
+  const userIdBase10 = parseInt(userId, 16);
+  const colorIndex = userIdBase10 % colors.length;
+
+  return (
+    <div
+      className={`w-8 h-8 rounded-full text-center flex items-center justify-center ${colors[colorIndex]}`}
+    >
+      <span className="opacity-50">{username[0].toUpperCase()}</span>
+    </div>
+  );
+};
+
+export default Avatar;
--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Avatar from './Avatar';
 
 const Chat = () => {
   const [ws, setWs] = useState(null);
@@ -50,8 +51,12 @@ const Chat = () => {
           Chat Panel
         </div>
         {Object.keys(onlineUsers).map((userId) => (
-          <div key={userId} className="border-b border-gray-100 py-2">
-            {onlineUsers[userId]}
+          <div
+            key={userId}
+            className="border-b border-gray-100 py-2 flex items-center gap-2"
+          >
+            <Avatar userId={userId} username={onlineUsers[userId]} />
+            <span>{onlineUsers[userId]}</span>
           </div>
         ))}
       </div>
