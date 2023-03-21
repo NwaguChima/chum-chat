b0830c6f7bd3f0e2b4728938b85fe0b1dfafb0da
NwaguChima <nwaguchima48@gmail.com>
2023-03-21 04:41:12 +0100

    added bar

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -55,12 +55,17 @@ const Chat = () => {
             <div
               onClick={() => handleSelectContact(userId)}
               key={userId}
-              className={`border-b border-gray-100 py-2 pl-4 flex items-center gap-2 cursor-pointer
+              className={`border-b border-gray-100 flex items-center gap-2 cursor-pointer
             transition duration-200 ease-in-out hover:bg-blue-50
-             ${selectedContact === userId ? 'bg-blue-50 pl-6' : ''}`}
+             ${selectedContact === userId ? 'bg-blue-50' : ''}`}
             >
-              <Avatar userId={userId} username={onlineUsers[userId]} />
-              <span className="text-gray-800">{onlineUsers[userId]}</span>
+              {userId === selectedContact && (
+                <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
+              )}
+              <div className="flex gap-2 py-3 pl-4 items-center">
+                <Avatar userId={userId} username={onlineUsers[userId]} />
+                <span className="text-gray-800">{onlineUsers[userId]}</span>
+              </div>
             </div>
           );
         })}
