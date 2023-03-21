6f102c258ee307ea5655ed3fe5c10d1ad0ee72e2
NwaguChima <nwaguchima48@gmail.com>
2023-03-21 04:51:44 +0100

    setup message send handler

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -10,6 +10,8 @@ const Chat = () => {
   const [selectedContact, setSelectedContact] = useState(null);
   const { username, id } = useContext(UserContext);
 
+  const [newMessageText, setNewMessageText] = useState('');
+
   useEffect(() => {
     if (!initialized.current) {
       initialized.current = true;
@@ -40,6 +42,23 @@ const Chat = () => {
 
   function handleSelectContact(userId) {
     setSelectedContact(userId);
+  }
+
+  function onInputChanged(ev) {
+    setNewMessageText(ev.target.value);
+  }
+
+  async function handleMessageSend(ev) {
+    ev.preventDefault();
+
+    const message = {
+      recipient: selectedContact,
+      from: id,
+      text: newMessageText,
+    };
+
+    ws.send(JSON.stringify(message));
+    setNewMessageText('');
   }
 
   return (
@@ -80,29 +99,36 @@ const Chat = () => {
             </div>
           )}
         </div>
-        <div className="flex gap-2">
-          <input
-            type="text"
-            className="bg-white border p-2 flex-grow rounded-sm"
-            placeholder="Type your message here..."
-          />
-          <button className="bg-blue-500 p-2 text-white rounded-sm">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
+        {!!selectedContact && (
+          <form className="flex gap-2" onSubmit={handleMessageSend}>
+            <input
+              type="text"
+              className="bg-white border p-2 flex-grow rounded-sm"
+              placeholder="Type your message here..."
+              value={newMessageText}
+              onChange={onInputChanged}
+            />
+            <button
+              className="bg-blue-500 p-2 text-white rounded-sm"
+              type="submit"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-              />
-            </svg>
-          </button>
-        </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                />
+              </svg>
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
